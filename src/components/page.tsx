add4d1616b53563cd9c0d4f89a8bf99e0f1432fb
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-12 19:37:01 +0000

    Updated styling and layout, changed p to text area for input, added button

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -10,26 +10,33 @@ export default function Page() {
         </div>
       </div>
       <div className="col-half flex column ac-c">
-        <h2>FlexBee</h2>
-        <p>
-          Your mission, should you choose to accept it is to make the bee do
-          bee things. You must achieve this by using flexbox to make the bee
-          get to his bee hive to continue doing bee stuff.
-        </p>
-        <div className="input-box flex">
-          <div className="number-bar">
-            <p className="line-num">1</p>
-            <p className="line-num">2</p>
-            <p className="line-num">3</p>
-            <p className="line-num">4</p>
-            <p className="line-num">5</p>
-            <p className="line-num">6</p>
+        <div className="row-half">
+          <div className="instructions">
+            <h2 className="font-c1 font-white">FlexBees</h2>
+            <p className="font-golos font-white">
+              Your mission, should you choose to accept it is to make the bee do
+              bee things. You must achieve this by using flexbox to make the bee
+              get to his bee hive to continue doing bee stuff.
+            </p>
           </div>
-          <div className="text-box">
-            <p className="line-text">.sky &#123;</p>
-            <p className="line-text pl-20">display: flex;</p>
-            <p className="line-text pl-20">filler text</p>
-            <p className="line-text">&#125;</p>
+        </div>
+        <div className="row-half">
+          <div className="input-box flex">
+            <div className="number-bar">
+              <p className="line-num">1</p>
+              <p className="line-num">2</p>
+              <p className="line-num">3</p>
+              <p className="line-num">4</p>
+              <p className="line-num">5</p>
+              <p className="line-num">6</p>
+            </div>
+            <div className="text-box">
+              <p className="line-text">.sky &#123;</p>
+              <p className="line-text pl-20">display: flex;</p>
+              <textarea className="line-text input-area ml-20"></textarea>
+              <p className="line-text">&#125;</p>
+              <button type="button" className="continue">Next</button>
+            </div>
           </div>
         </div>
       </div>
