13b02df5ffa924462a5880ca88e1093e004fad63
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-13 00:59:05 +0000

    Modified page to take question and answer to style display and created animations

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import JustifyHelp from "./justify-help";
 import AlignHelp from "./align-help";
-import { checkAnswer, convertClassToReact, removeSemiColon, checkFlex } from "../lib";
+import { checkAnswer, convertClassToReact, removeSemiColon, checkFlex, answers } from "../lib";
 
 export default function Page() {
   const [userAnswer, setUserAnswer] = useState('');
   const [level, setLevel] = useState(1);
+  const levelDescription: string = answers.get(level).levelDescription;
+  const splitFormat: string[] = answers.get(level).levelAnswer.split(':');
+  const formatProperty: string = convertClassToReact(splitFormat[0]);
+  const hiveStyle = {
+    [formatProperty]: splitFormat[1]
+  }
 
-  const isCorrect = checkAnswer(level, userAnswer)
-  const splitAnswer = userAnswer.split(':');
+  const isCorrect: boolean = checkAnswer(level, userAnswer)
+  const splitAnswer: string[] = userAnswer.split(':');
   const property: string | undefined = convertClassToReact(splitAnswer[0]);
   const value: string | undefined = removeSemiColon(splitAnswer[1]);
   const checkedValue: string = checkFlex(value);
@@ -23,7 +29,7 @@ export default function Page() {
       <div className="col-half flex jc-c ac-c">
         <div className="sky flex" style={userStyle}>
           <img className="bee" src="/images/bee-g.webp" />
-          <div className="hive-container flex jc-fe">
+          <div className="hive-container flex" style={hiveStyle}>
             <img className="hive" src="/images/hive-g.webp"/>
           </div>
         </div>
@@ -33,9 +39,7 @@ export default function Page() {
           <div className="instructions">
             <h1 className="font-c1 font-white title">FlexBees</h1>
             <p className="font-golos font-white">
-              Your mission, should you choose to accept it is to make the bee do
-              bee things. You must achieve this by using flexbox to make the bee
-              get to his bee hive to continue doing bee stuff.
+              { levelDescription }
             </p>
           </div>
         </div>
@@ -52,10 +56,10 @@ export default function Page() {
             <div className="text-box">
               <p className="line-text font-consolas">.sky &#123;</p>
               <p className="line-text pl-20 font-consolas">display: flex;</p>
-              <textarea onChange={ event => setUserAnswer(event.target.value) } className="line-text input-area ml-20"></textarea>
+              <textarea onChange={ event => setUserAnswer(event.target.value) } className="line-text input-area ml-20" value={userAnswer}></textarea>
 
               <p className="line-text font-consolas">&#125;</p>
-              <button disabled={isCorrect ? false : true} onClick={() => setLevel(level + 1)} type="button" className="continue">Next</button>
+              <button disabled={isCorrect ? false : true} onClick={() => {setLevel(level + 1); setUserAnswer('')}} type="button" className={`continue ${isCorrect}`}>Next</button>
             </div>
           </div>
         </div>
--- src/lib/answers.ts
+++ src/lib/answers.ts
@@ -1,4 +1,5 @@
 const answers = new Map();
 answers.set(1, { levelDescription: 'text', levelAnswer: 'justify-content: flex-end' });
+answers.set(2, { levelDescription: 'texting', levelAnswer: 'justify-content: center' });
 
 export default answers;
